8bb99b35b436f67e1243e52f4a031618a45041a5
TES <taaviaro@student.uef.fi>
2025-05-10 12:01:56 +0300

    Saturday

--- fluff-update.sh
+++ fluff-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 date=$(date +%d/%M/%y)
 epoch_time=$(date +%s)
-message=$(date +A)
+message=$(date +%A)
 echo "$date epoch: $epoch_time" >> fluff.txt
 fortune | cowsay >> fluff.txt
 echo "" >> fluff.txt
-git commit +m "$message"
-git push
+#git commit -m "$message"
+#git push
