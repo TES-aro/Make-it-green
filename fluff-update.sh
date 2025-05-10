ccb5a2d2b53b8d009b09d395168cd29c1484f09f
TES <taaviaro@student.uef.fi>
2025-05-10 15:40:34 +0300

    I like unicorns

--- fluff-update.sh
+++ fluff-update.sh
@@ -7,11 +7,11 @@ if [ $X == $Y ]; then
 	date=$(date +%d/%M/%y)
 	epoch_time=$(date +%s)
 	message=$(date +%A)
-	# customize this for what ever you wish to upload
+# customize this for what ever you wish to upload
 	echo "$date epoch: $epoch_time" >> fluff.txt
 	fortune | cowsay >> fluff.txt
 	echo "" >> fluff.txt
-	# pushes the update
+# pushes the update
 	git add .
 	git commit -m "$message"
 	git push
