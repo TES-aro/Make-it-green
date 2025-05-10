3b06db8ce0b5af0bc0a4e527613b5ffae9c6ece9
TES <taaviaro@student.uef.fi>
2025-05-10 15:02:00 +0300

    I like unicorns

--- fluff-update.sh
+++ fluff-update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # check if has updated today
-date=$(date +%d/%M/%y)
-epoch_time=$(date +%s)
-message=$(date +%A)
-# customize this for what ever you wish to upload
-echo "$date epoch: $epoch_time" >> fluff.txt
-fortune | cowsay >> fluff.txt
-echo "" >> fluff.txt
-# pushes the update
-git add .
-git commit -m "I like unicorns"
-git push
+X="$(date +%e)"
+Y="$(date +%e -r fluff.txt)"
+if [ $X == $Y ]; then
+
+	date=$(date +%d/%M/%y)
+	epoch_time=$(date +%s)
+	message=$(date +%A)
+	# customize this for what ever you wish to upload
+	echo "$date epoch: $epoch_time" >> fluff.txt
+	fortune | cowsay >> fluff.txt
+	echo "" >> fluff.txt
+	# pushes the update
+	git add .
+	git commit -m "I like unicorns"
+	git push
+fi
