017bb29a7f20f145a1d783859c4f2d39fb5f881d
TES <taaviaro@student.uef.fi>
2025-05-10 12:03:33 +0300

    git is hard :(

--- fluff-update.sh
+++ fluff-update.sh
@@ -5,5 +5,5 @@ message=$(date +%A)
 echo "$date epoch: $epoch_time" >> fluff.txt
 fortune | cowsay >> fluff.txt
 echo "" >> fluff.txt
-#git commit -m "$message"
-#git push
+git commit -m "$message"
+git push
